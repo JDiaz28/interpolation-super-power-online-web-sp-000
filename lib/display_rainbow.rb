3bb6e9045a57631ba2e5d196d0a2b016415e2000
Abraham Moreno <adiaz2821@gmail.com>
2020-04-13 15:36:16 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-puts "color#{[0]}, #{[1]}, #{[2]}, #{[3]}, #{[4]}, #{[5]}, #{[6]}"
+puts "color: #{[0]}, #{[1]}, #{[2]}, #{[3]}, #{[4]}, #{[5]}, #{[6]}"
 end 
